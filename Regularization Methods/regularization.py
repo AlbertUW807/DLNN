0f6999a24a682d6ab1ac293c03b9c73b1b77d7ae
Albert Ong <fluke807@gmail.com>
2020-05-01 00:17:07 -0400

    Updated Regularization Methods Repo

---
+++ Regularization Methods/regularization.py
@@ -0,0 +1,57 @@
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from reg_utils import sigmoid, relu, plot_decision_boundary, initialize_parameters, load_2D_dataset, predict_dec
+from reg_utils import compute_cost, predict, forward_propagation, backward_propagation, update_parameters
+import sklearn
+import sklearn.datasets
+import scipy.io
+from testCases import *
+
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (7.0, 4.0)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# Loading data sets
+train_X, train_Y, test_X, test_Y = load_2D_dataset()
+
+# Three-Layer Neural Network: LINEAR->RELU->LINEAR->RELU->LINEAR->SIGMOID
+def model(X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True, lambd = 0, keep_prob = 1):
+    grads = {}
+    costs = []
+    m = X.shape[1]
+    layers_dims = [X.shape[0], 20, 3, 1]
+    
+    parameters = initialize_parameters(layers_dims)
+
+    for i in range(0, num_iterations):
+        if keep_prob == 1:
+            a3, cache = forward_propagation(X, parameters)
+        elif keep_prob < 1:
+            a3, cache = forward_propagation_with_dropout(X, parameters, keep_prob)
+        
+        if lambd == 0:
+            cost = compute_cost(a3, Y)
+        else:
+            cost = compute_cost_with_regularization(a3, Y, parameters, lambd)
+
+        assert(lambd==0 or keep_prob==1)    
+
+        if lambd == 0 and keep_prob == 1:
+            grads = backward_propagation(X, Y, cache)
+        elif lambd != 0:
+            grads = backward_propagation_with_regularization(X, Y, cache, lambd)
+        elif keep_prob < 1:
+            grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+    
+    # plot the cost
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (x1,000)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
